b0548cda4b28913e02ff92611096ee3bab4005c0
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-01-04 18:32:51 +0900

    DPの例2

--- knapsack.cpp
+++ knapsack.cpp
@@ -13,12 +13,12 @@ int W;
 int dp[MAX + 1][MAX + 1];
 
 void solve() {
-    for (int i = N - 1; i >= 0; --i) {
+    for (int i = 0; i < N; ++i) {
         for (int j = 0; j <= W; ++j) {
             if (j < w[i]) {
-                dp[i][j] = dp[i + 1][j];
+                dp[i + 1][j] = dp[i][j];
             } else {
-                dp[i][j] = max(dp[i + 1][j], dp[i + 1][j - w[i]] + v[i]);
+                dp[i + 1][j] = max(dp[i][j], dp[i][j - w[i]] + v[i]);
             }
         }
     }
@@ -35,7 +35,7 @@ int main() {
 
     solve();
 
-    printf("%d\n", dp[0][W]);
+    printf("%d\n", dp[N][W]);
 
     return 0;
 }
