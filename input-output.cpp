288e48b3b296af0ba051c749805ec40cc9a7ba9c
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-01-08 16:42:34 +0900

    入出力の例（更新）
    シンプルで再利用しやすいものに更新した

--- input-output.cpp
+++ input-output.cpp
@@ -2,29 +2,11 @@
 
 using namespace std;
 
-const int MAX = 10;
-int n, m;
-int a[MAX], b[MAX];
-
 int main() {
 
-    scanf("%d %d", &n, &m);
-
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", &a[i]);
-    }
-
-    for (int i = 0; i < m; ++i) {
-        scanf("%d", &b[i]);
-    }
-
-    for (int i = 0; i < n; ++i) {
-        printf("a[%d] = %d\n", i, a[i]);
-    }
-
-    for (int i = 0; i < m; ++i) {
-        printf("b[%d] = %d\n", i, b[i]);
-    }
+    int N;
+    scanf("%d", &N);
+    printf("%d\n", N);
 
     return 0;
 }
