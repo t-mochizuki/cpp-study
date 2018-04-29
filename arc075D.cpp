c8eb8a74084cf842a85340e145de2b287f1b5790
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-04-29 10:50:56 +0900

    [wip] ARC 075 D

---
+++ arc075D.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define REP(i, n) for (int i = 0; i < n; ++i)
+
+int main() {
+    int N, A, B;
+    scanf("%d %d %d", &N, &A, &B);
+
+    int h[N];
+    REP(i, N) {
+        scanf("%d", &h[i]);
+    }
+
+    bool done = false;
+    int cnt = 0;
+    while (true) {
+        sort(h, h + N, greater<int>());
+        REP(i, N) {
+            if (i == 0) h[0] -= A;
+            else h[i] -= B;
+        }
+
+        done = true;
+        REP(i, N) {
+            if (h[i] > 0) {
+                done = false;
+                break;
+            }
+        }
+
+        cnt++;
+
+        if (done) break;
+    }
+
+    printf("%d\n", cnt);
+
+    return 0;
+}
