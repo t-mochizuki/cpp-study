b21f1bf42bc505314d84f3f6dfebb68fd83cce90
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-04-30 13:44:39 +0900

    Add ABC 015 D

--- abc015D.cpp
+++ abc015D.cpp
@@ -11,13 +11,16 @@ int N; // スクリーンショットの数, 50
 vector<int> v;
 int A[MAX_N]; // 幅
 int B[MAX_N]; // 優先度
-bool done[MAX_N][MAX_N][MAX_W];
-int memo[MAX_N][MAX_N][MAX_W];
+bool done[MAX_N + 1][MAX_N + 1][MAX_W + 1];
+int memo[MAX_N + 1][MAX_N + 1][MAX_W + 1];
 
+// i 番目以降のスクリーンショットで、枚数の総和が k 以下、幅の総和が w 以下となるように選ぶ
 int search(int i, int k, int w) {
     if (done[i][k][w]) return memo[i][k][w];
 
     if (i == N) {
+        done[i][k][w] = true;
+        memo[i][k][w] = 0;
         return 0;
     } else if (w < A[i] || k == 0) {
         int res = search(i + 1, k, w);
@@ -43,9 +46,9 @@ int main() {
         scanf("%d %d", &A[i], &B[i]);
     }
 
-    REP(i, N) {
-        REP(j, K) {
-            REP(k, W) {
+    REP(i, N + 1) {
+        REP(j, K + 1) {
+            REP(k, W + 1) {
                 done[i][j][k] = false;
             }
         }
