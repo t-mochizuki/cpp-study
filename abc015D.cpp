4903ef9af0e64a118022a322aa04d010f7cae587
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-04-30 13:32:48 +0900

    [wip] ABC 015 D

--- abc015D.cpp
+++ abc015D.cpp
@@ -6,20 +6,31 @@ using namespace std;
 #define REP(i, n) for (int i = 0; i < n; ++i)
 
 const int MAX_N = 50;
+const int MAX_W = 1e4;
 int N; // スクリーンショットの数, 50
 vector<int> v;
 int A[MAX_N]; // 幅
 int B[MAX_N]; // 優先度
+bool done[MAX_N][MAX_N][MAX_W];
+int memo[MAX_N][MAX_N][MAX_W];
 
 int search(int i, int k, int w) {
+    if (done[i][k][w]) return memo[i][k][w];
+
     if (i == N) {
         return 0;
     } else if (w < A[i] || k == 0) {
-        return search(i + 1, k, w);
+        int res = search(i + 1, k, w);
+        done[i][k][w] = true;
+        memo[i][k][w] = res;
+        return res;
     } else {
         int res1 = search(i + 1, k - 1, w - A[i]) + B[i];
         int res2 = search(i + 1, k, w);
-        return max(res1, res2);
+        int res = max(res1, res2);
+        done[i][k][w] = true;
+        memo[i][k][w] = res;
+        return res;
     }
 }
 
@@ -30,6 +41,14 @@ int main() {
     scanf("%d %d", &N, &K);
     REP(i, N) {
         scanf("%d %d", &A[i], &B[i]);
+    }
+
+    REP(i, N) {
+        REP(j, K) {
+            REP(k, W) {
+                done[i][j][k] = false;
+            }
+        }
     }
 
     int res = search(0, K, W);
