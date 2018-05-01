f4e29d3a63d6763252c2128018a5b3e92f6ee2f0
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-05-02 00:50:45 +0900

    Add ARC 074 D

--- arc074D.cpp
+++ arc074D.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <queue>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
-#define REP(i, a, n) for (int i = a; i < n; ++i)
+#define REP(a, i, n) for (int i = a; i < n; ++i)
+#define RREP(n, i, a) for (int i = n - 1; i >= a; --i)
 
 int main() {
     int N;
@@ -12,42 +14,60 @@ int main() {
 
     int _3N = 3 * N;
     int a[_3N];
-    REP(i, 0, _3N) {
+    REP(0, i, _3N) {
         scanf("%d", &a[i]);
     }
 
+    // Red
     priority_queue<int, vector<int>, greater<int> > red_q;
     long red_sum = 0;
-    REP(i, 0, N) {
+    REP(0, i, N) {
         red_q.push(a[i]);
         red_sum += a[i];
     }
-
+    long red_sum_v[_3N];
+    red_sum_v[N - 1] = red_sum;
 
     int _2N = 2 * N;
-    priority_queue<int> blue_q;
-    long blue_sum = 0;
-    REP(i, _2N, _3N) {
-        blue_q.push(a[i]);
-        blue_sum += a[i];
-    }
-
-    REP(i, N, _2N) {
+    REP(N, i, _2N) {
         long ai = a[i];
         long red_t = red_q.top();
-        long blue_t = blue_q.top();
         if (ai > red_t) {
             red_q.pop();
             red_q.push(ai);
             red_sum += ai - red_t;
-        } else if (ai < blue_t) {
+        }
+        red_sum_v[i] = red_sum;
+    }
+
+    // Blue
+    priority_queue<int> blue_q;
+    long blue_sum = 0;
+    REP(_2N, i, _3N) {
+        blue_q.push(a[i]);
+        blue_sum += a[i];
+    }
+    long blue_sum_v[_3N];
+    blue_sum_v[_2N] = blue_sum;
+
+    RREP(_2N, i, N) {
+        long ai = a[i];
+        long blue_t = blue_q.top();
+        if (ai < blue_t) {
             blue_q.pop();
             blue_q.push(ai);
             blue_sum += ai - blue_t;
         }
+        blue_sum_v[i] = blue_sum;
     }
 
-    printf("%ld\n", red_sum - blue_sum);
+    long ans = LONG_MIN;
+    REP(N, i, _2N + 1) {
+        // printf("%ld - %ld = %ld\n", red_sum_v[i - 1], blue_sum_v[i], red_sum_v[i - 1] - blue_sum_v[i]);
+        ans = max(ans, red_sum_v[i - 1] - blue_sum_v[i]);
+    }
+
+    printf("%ld\n", ans);
 
     return 0;
 }
