001c181c5d8b5c4d562d82fc8c498740edc5f6a9
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-05-13 16:36:30 +0900

    Add template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+
+#define rep(a, i, n) for (int i = a; i <= n; ++i)
+
+int main() {
+    return 0;
+}
