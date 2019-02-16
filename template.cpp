9c583117f9b3630ec3e184d0e147c81f8875eac7
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2019-02-16 18:07:46 +0900

    Add Educational DP Contest A

--- template.cpp
+++ template.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 #define REP(a, i, n) for (int i = a; i < n; ++i)
+
 #define RREP(a, i, n) for (int i = n - 1; i >= 0; --i)
 
 int max(int X, int Y) {
@@ -13,8 +14,8 @@ int min(int X, int Y) {
     return X < Y ? X : Y;
 }
 
-int abs(int a, int b) {
-    return max(b - a, a - b);
+int abs(int X, int Y) {
+    return X > Y ? X - Y : Y - X;
 }
 
 int main() {
