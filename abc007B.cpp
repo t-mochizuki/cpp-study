97b7db11ef6f32187ff945fd6022563709eece7a
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2019-02-17 15:07:23 +0900

    Update ABC 007

---
+++ abc007B.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string A;
+    cin >> A;
+
+    if ("a" == A) {
+        printf("-1\n");
+    } else {
+        printf("a\n");
+    }
+
+    return 0;
+}
