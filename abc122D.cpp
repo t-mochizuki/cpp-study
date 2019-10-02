8644760a6f8f5b44e952fd4d3d6cbd839466dede
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2019-10-03 01:35:25 +0900

    Update ABC 122

--- abc122D.cpp
+++ abc122D.cpp
@@ -1,69 +1,61 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
 
-// TODO: RETRY
-
 #define REP(a, i, n) for (int i = a; i < n; ++i)
 
-long ans = 0;
+const int MOD = 1e9 + 7;
+const string AGC = "AGC";
+const string ACGT[4] = {"A", "C", "G", "T"};
+map<string, int> table;
+int memo[101][64];
 
-void dfs(int i, int N, char* s) {
-    if (i - 2 > 0) {
-        if ((s[i - 3] == 'A' && s[i - 2] == 'G' && s[i - 1] == 'C')
-            || (s[i - 3] == 'A' && s[i - 2] == 'C' && s[i - 1] == 'G')
-            || (s[i - 3] == 'G' && s[i - 2] == 'A' && s[i - 1] == 'C')) {
-            s[i] = '\0';
-            return ;
+bool ok(string last4) {
+    REP(0, i, 4) {
+        string tmp = last4;
+        if (i >= 1) {
+            char c = tmp[i - 1];
+            tmp[i - 1] = tmp[i];
+            tmp[i] = c;
+        }
+
+        if (tmp.find(AGC) != string::npos) {
+            return false;
         }
     }
+    return true;
+}
 
-    if (i - 3 > 0) {
-        if ((s[i - 4] == 'A' && s[i - 3] == 'G' && s[i - 1] == 'C')
-            || (s[i - 4] == 'A' && s[i - 2] == 'G' && s[i - 1] == 'C')) {
-            s[i] = '\0';
-            return ;
+int dfs(int N, int cur, string last3) {
+    if (memo[cur][table[last3]] != 0) {
+        return memo[cur][table[last3]];
+    }
+
+    if (cur == N) {
+        return 1;
+    }
+
+    int ret = 0;
+    REP(0, i, 4) {
+        if (ok(last3 + ACGT[i])) {
+            ret = (ret + dfs(N, cur + 1, last3.substr(1) + ACGT[i])) % MOD;
         }
     }
-
-    if (i == N) {
-        s[i] = '\0';
-        // cout << s << endl;
-        ans++;
-        return ;
-    }
-
-    REP(0, j, 4) {
-        if (j == 0) {
-            s[i] = 'A';
-            dfs(i + 1, N, s);
-        } else if (j == 1) {
-            s[i] = 'C';
-            dfs(i + 1, N, s);
-        } else if (j == 2) {
-            s[i] = 'G';
-            dfs(i + 1, N, s);
-        } else {
-            s[i] = 'T';
-            dfs(i + 1, N, s);
-        }
-    }
-
+    memo[cur][table[last3]] = ret;
+    return ret;
 }
 
 int main() {
     int N; cin >> N;
-
-    if (N > 14) {
-        cout << 0 << endl;
-        return 0;
+    REP(0, i, 101) REP(0, j, 64) memo[i][j] = 0;
+    int count = 0;
+    REP(0, i, 4) REP(0, j, 4) REP(0, k, 4) {
+        table.insert(make_pair(ACGT[i] + ACGT[j] + ACGT[k], count));
+        count++;
     }
-
-    char s[N + 1];
-    dfs(0, N, s);
-
-    cout << ans << endl;
-
+    cout << dfs(N, 0, string("TTT")) << endl;
     return 0;
 }
