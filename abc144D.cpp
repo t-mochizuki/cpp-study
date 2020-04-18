737c362504183a9da28056eabd97e06409796dfc
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-04-18 21:15:24 +0900

    Update ABC 144 D

--- abc144D.cpp
+++ abc144D.cpp
@@ -14,6 +14,7 @@ double f(double a, double b, double c) {
     if (a * tan(c) <= b) {
         return a * a * b - a * a * a * tan(c) / 2.0;
     } else {
+        // tan(M_PI / 2.0 - c) == 1 / tan(c)
         return b * b / tan(c) * a / 2.0;
     }
 }
