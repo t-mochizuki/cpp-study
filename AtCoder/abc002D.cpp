48d08dc3e498d20ff0cbf1a94ffb03a662ab1880
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-04-21 22:05:17 +0900

    Add ABC 002 D
    I read the section D of editorial.

--- AtCoder/abc002D.cpp
+++ AtCoder/abc002D.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <iostream>
+#include <map>
+#include <vector>
 
 using namespace std;
 
@@ -13,17 +15,36 @@ int main() {
     int n, m; cin >> n >> m;
 
     // 派閥
-    int w = pos(2, n);
-    int b[w][n];
+    int w = pow(2, n);
+    map<int, vector<int> > mv;
     // from 1 to 2^12-1
     for (int j = 1; j < w; ++j) {
-        for (int i = 0; i < n; ++i) {
-            b[j][i] = ((j >> i) & 0x1);
+        REP(0, i, n) {
+            if (((j >> i) & 0x1) == 1) {
+                map<int, vector<int> >::iterator it = mv.find(j);
+                if (it != mv.end()) {
+                    mv.at(j).push_back(i);
+                } else {
+                    vector<int> v; v.push_back(i);
+                    mv.insert(make_pair(j, v));
+                }
+            }
         }
     }
 
+    // DEGUB
+    // for (int j = 1; j < w; ++j) {
+    //     cout << "j=" << j << endl;
+    //     vector<int> v = mv.at(j);
+    //     for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+    //         if (it == v.begin()) cout << (*it);
+    //         else cout << " " << (*it);
+    //     }
+    //     cout << endl;
+    // }
+
     // 隣接行列
-    int a[n][n]
+    int a[n][n]; REP(0, i, n) REP(0, j, n) a[i][j] = 0;
     REP(0, i, m) {
         int x, y; cin >> x >> y;
         x--; y--; // zero origin
@@ -31,15 +52,37 @@ int main() {
         a[y][x] = 1;
     }
 
+    // DEGUB
+    // REP(0, i, n) {
+    //     REP(0, j, n) {
+    //         if (j == 0) {
+    //             cout << a[i][j];
+    //         } else {
+    //             cout << " " << a[i][j];
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
     int ans = 0;
     for (int j = 1; j < w; ++j) {
-        for (int i = 0; i < n; ++i) {
-            for (int k = 0; k < n; ++k) {
-                if (b[j][i] == 1 && a[i][k] == 1 && b[j][k] == 1) {
+        vector<int> v = mv.at(j);
+        bool ok = true;
+        REP(0, x, v.size()) {
+            REP(0, y, v.size()) {
+                if (x == y) continue;
+
+                if (a[v[x]][v[y]] != 1) {
+                    ok = false;
                 }
             }
         }
+        if (ok) {
+            ans = ans > v.size() ? ans : v.size();
+        }
     }
+
+    cout << ans << endl;
 
     return 0;
 }
