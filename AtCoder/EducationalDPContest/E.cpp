bb1c7b325ca50563d1cdd45995ef7c9e0b822a63
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-04-25 15:19:20 +0900

    Add Educational DP Contest E

---
+++ AtCoder/EducationalDPContest/E.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+#define REP(a, i, n) for (int i = a; i < n; ++i)
+
+struct data_t {
+    int w, v;
+};
+
+int min(int X, int Y) {
+    return X < Y ? X : Y;
+}
+
+int main() {
+    int N, w; cin >> N >> w; // N <= 100, w <= 1e9
+    data_t I[N + 1];
+    for (int i = 1; i <= N; ++i) {
+        cin >> I[i].w; // w <= 1e9
+        cin >> I[i].v; // v <= 1000
+    }
+
+    long W[N + 1][100000 + 1];
+
+    for (int v = 0; v <= 100000; ++v) {
+        W[0][v] = 100000000001; // 1e9 * 100 + 1
+    }
+
+    for (int i = 1; i <= N; ++i) {
+        for (int v = 0; v <= 100000; ++v) {
+            if (I[i].v >= v) {
+                W[i][v] = min(I[i].w, W[i - 1][v]);
+            } else {
+                W[i][v] = min(W[i - 1][v], W[i - 1][v - I[i].v] + I[i].w);
+            }
+        }
+    }
+
+    // DEBUG
+    // for (int i = 1; i <= N; ++i) {
+    //     for (int v = 29; v <= 51; ++v) {
+    //         if (v == 29) {
+    //             cout << W[i][v];
+    //         } else {
+    //             cout << " " << W[i][v];
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    int ans = 0;
+    for (int i = 1; i <= N; ++i) {
+        for (int v = 0; v <= 100000; ++v) {
+            if (W[i][v] > w) break;
+            ans = max(ans, v);
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
