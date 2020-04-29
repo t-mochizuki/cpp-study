7ba17a4ec4cd57e558785d42e121013d41758ce3
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-04-29 21:28:59 +0900

    Update ABC 004 D
    I read the section D of editorial.

--- AtCoder/マーブル.cpp
+++ AtCoder/マーブル.cpp
@@ -3,34 +3,67 @@
 
 using namespace std;
 
-#define REP(a, i, n) for (int i = a; i < n; ++i)
+const int INDEFINITE = 1000000007;
+
+// 最小の移動数
+int m[2001][901];
+
+template<class T> inline T _abs(T X) {
+    return X > 0 ? X : -X;
+}
+
+template<class T> inline T _min(T X, T Y) {
+    return X < Y ? X : Y;
+}
+
+int dp(int pos, int r, int g, int b) {
+    int rgb = r + g + b;
+    if (rgb == 0) {
+        return 0;
+    }
+    int index = pos + 1000;
+    if (index == 2001) {
+        return INDEFINITE - 1;
+    }
+
+    if (m[index][rgb] != INDEFINITE) {
+        return m[index][rgb];
+    }
+
+    int takeIt, leaveIt;
+    if (r != 0) {
+        takeIt = dp(pos + 1, r - 1, g, b) + _abs(pos - (-100));
+        leaveIt = dp(pos + 1, r, g, b);
+    } else if (g != 0) {
+        takeIt = dp(pos + 1, r, g - 1, b) + _abs(pos);
+        leaveIt = dp(pos + 1, r, g, b);
+    } else if (b != 0) {
+        takeIt = dp(pos + 1, r, g, b - 1) + _abs(pos - 100);
+        leaveIt = dp(pos + 1, r, g, b);
+    } else {
+        terminate();
+    }
+    m[index][rgb] = _min(takeIt, leaveIt);
+
+    return m[index][rgb];
+}
 
 int main() {
     int r, g, b; cin >> r >> g >> b;
-    r--, g--, b--;
+    for (int i = 0; i <= 2000; ++i) {
+        for (int j = 0; j <= 900; ++j) {
+            m[i][j] = INDEFINITE;
+        }
+    }
 
-    int ans, w; ans = 0; w = 1;
-    REP(0, i, r) {
-        if (i % 2 == 0 && i != 0) {
-            w++;
-        }
-        ans += w;
-    }
-    w = 1;
-    REP(0, i, g) {
-        if (i % 2 == 0 && i != 0) {
-            w++;
-        }
-        ans += w;
-    }
-    w = 1;
-    REP(0, i, b) {
-        if (i % 2 == 0 && i != 0) {
-            w++;
-        }
-        ans += w;
+    dp(-1000, r, g, b);
+
+    int ans = INDEFINITE;
+    for (int i = 0; i <= 2000; ++i) {
+        ans = _min(ans, m[i][r + g + b]);
     }
 
     cout << ans << endl;
+
     return 0;
 }
