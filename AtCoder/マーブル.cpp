b6dc38550ba386562571796f39cceb7ef8a9bc74
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-04-29 23:42:24 +0900

    Update ABC 004 D
    I use `using std::cin` instead of `using namespace std`.

--- AtCoder/マーブル.cpp
+++ AtCoder/マーブル.cpp
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <iostream>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::terminate;
 
 const int INDEFINITE = 1000000007;
 
 // 最小の移動数
 int m[2001][901];
 
-template<class T> inline T _abs(T X) {
+template<class T> inline T abs(T X) {
     return X > 0 ? X : -X;
 }
 
-template<class T> inline T _min(T X, T Y) {
+template<class T> inline T min(T X, T Y) {
     return X < Y ? X : Y;
 }
 
@@ -32,18 +35,18 @@ int dp(int pos, int r, int g, int b) {
 
     int takeIt, leaveIt;
     if (r != 0) {
-        takeIt = dp(pos + 1, r - 1, g, b) + _abs(pos - (-100));
+        takeIt = dp(pos + 1, r - 1, g, b) + abs(pos - (-100));
         leaveIt = dp(pos + 1, r, g, b);
     } else if (g != 0) {
-        takeIt = dp(pos + 1, r, g - 1, b) + _abs(pos);
+        takeIt = dp(pos + 1, r, g - 1, b) + abs(pos);
         leaveIt = dp(pos + 1, r, g, b);
     } else if (b != 0) {
-        takeIt = dp(pos + 1, r, g, b - 1) + _abs(pos - 100);
+        takeIt = dp(pos + 1, r, g, b - 1) + abs(pos - 100);
         leaveIt = dp(pos + 1, r, g, b);
     } else {
         terminate();
     }
-    m[index][rgb] = _min(takeIt, leaveIt);
+    m[index][rgb] = min(takeIt, leaveIt);
 
     return m[index][rgb];
 }
@@ -60,7 +63,7 @@ int main() {
 
     int ans = INDEFINITE;
     for (int i = 0; i <= 2000; ++i) {
-        ans = _min(ans, m[i][r + g + b]);
+        ans = min(ans, m[i][r + g + b]);
     }
 
     cout << ans << endl;
