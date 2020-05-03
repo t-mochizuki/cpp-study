f2039febdc40d84a3328629100b10cd855487554
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-05-03 16:07:10 +0900

    Update Round 1C 2020 - Code Jam 2020
    I read the section Overrandomized of ANALYSIS.
    I passed Test Set 2, but got WA in Test Set 3.

--- CodeJam/Round 1C 2020/Overrandomized.cpp
+++ CodeJam/Round 1C 2020/Overrandomized.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <set>
 // #include <fstream>
 
 using std::cin;
@@ -11,18 +11,18 @@ using std::endl;
 using std::terminate;
 using std::string;
 using std::vector;
-using std::sort;
+using std::set;
 
 struct Record {
-    int M;
+    string M;
     string R;
     bool operator<(const Record& rhs) const {
-        return M < rhs.M;
+        return M.size() < rhs.M.size();
     }
 };
 
 int main() {
-    // std::ifstream in("input");
+    // std::ifstream in("Overrandomized.input");
     // cin.rdbuf(in.rdbuf());
 
     int t; cin >> t;
@@ -30,44 +30,45 @@ int main() {
         int U; cin >> U;
 
         vector<Record> v;
+        set<char> s;
         for (int i = 0; i < 10000; ++i) {
-            int M; cin >> M;
+            string M; cin >> M;
             string R; cin >> R;
-            v.push_back((Record){M, R});
+            if (M.size() == R.size()) {
+                v.push_back((Record){M, R});
+            }
+            if (s.size() != 10) {
+                for (int j = 0; j < R.size(); ++j) {
+                    s.insert(R[j]);
+                }
+            }
         }
-
-        sort(v.begin(), v.end());
 
         char D[11];
         for (vector<Record>::iterator it = v.begin(); it != v.end(); ++it) {
-            if (it->M == 1) {
+            if (it->M.front() == '1') {
                 D[1] = it->R.front();
-            } else if (it->M == 10) {
-                if (it->R.size() == 2 && it->R.front() == D[1]) {
-                    // cout << "DEGUB: R=" << it->R << endl;
-                    D[0] = it->R.back();
-                }
-            } else if (it->M == 2) {
+            } else if (it->M.front() == '2') {
                 if (it->R.front() != D[1]) {
                     D[2] = it->R.front();
                 }
-            } else if (it->M == 3) {
+            } else if (it->M.front() == '3') {
                 if (it->R.front() != D[1] && it->R.front() != D[2]) {
                     D[3] = it->R.front();
                 }
-            } else if (it->M == 4) {
+            } else if (it->M.front() == '4') {
                 if (it->R.front() != D[1] && it->R.front() != D[2] && it->R.front() != D[3]) {
                     D[4] = it->R.front();
                 }
-            } else if (it->M == 5) {
+            } else if (it->M.front() == '5') {
                 if (it->R.front() != D[1] && it->R.front() != D[2] && it->R.front() != D[3] && it->R.front() != D[4]) {
                     D[5] = it->R.front();
                 }
-            } else if (it->M == 6) {
+            } else if (it->M.front() == '6') {
                 if (it->R.front() != D[1] && it->R.front() != D[2] && it->R.front() != D[3] && it->R.front() != D[4] && it->R.front() != D[5]) {
                     D[6] = it->R.front();
                 }
-            } else if (it->M == 7) {
+            } else if (it->M.front() == '7') {
                 if (it->R.front() != D[1]
                         && it->R.front() != D[2]
                         && it->R.front() != D[3]
@@ -76,7 +77,7 @@ int main() {
                         && it->R.front() != D[6]) {
                     D[7] = it->R.front();
                 }
-            } else if (it->M == 8) {
+            } else if (it->M.front() == '8') {
                 if (it->R.front() != D[1]
                         && it->R.front() != D[2]
                         && it->R.front() != D[3]
@@ -86,7 +87,7 @@ int main() {
                         && it->R.front() != D[7]) {
                     D[8] = it->R.front();
                 }
-            } else if (it->M == 9) {
+            } else if (it->M.front() == '9') {
                 if (it->R.front() != D[1]
                         && it->R.front() != D[2]
                         && it->R.front() != D[3]
@@ -101,6 +102,20 @@ int main() {
                 continue;
             }
         }
+
+        for (decltype(s)::iterator it = s.begin(); it != s.end(); ++it) {
+            bool notFound = true;
+            for (int i = 1; i <= 9; ++i) {
+                if (D[i] == (*it)) {
+                    notFound = false;
+                }
+            }
+            if (notFound) {
+                D[0] = (*it);
+                break;
+            }
+        }
+
         D[10] = '\0';
 
         printf("Case #%d: %s\n", x, D);
