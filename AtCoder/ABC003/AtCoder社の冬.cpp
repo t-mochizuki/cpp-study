ab8b4dfb5c9d3dfb3bce0974650e6528ebf7c2f0
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-05-06 00:28:47 +0900

    wip

--- AtCoder/ABC003/AtCoder社の冬.cpp
+++ AtCoder/ABC003/AtCoder社の冬.cpp
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::terminate;
 
 #define REP(a, i, n) for (int i = a; i < n; ++i)
 
+const long MOD = 1000000007;
 long memo[901][901];
 
 long pascal(long i, long j) {
     if (memo[i][j] != 0) return memo[i][j];
 
-    memo[i][j] = (pascal(i - 1, j - 1) + pascal(i - 1, j)) % 1000000007;
+    memo[i][j] = (pascal(i - 1, j - 1) + pascal(i - 1, j)) % MOD;
 
     return memo[i][j];
 }
 
-int main() {
+long func(long x, long y, long d, long l) {
+    if (x < 0 || y < 0) return 0;
+    return pascal(x * y, d) * pascal(x * y - d, l) % MOD;
+}
+
+void solve() {
     int r, c; cin >> r >> c;
     int x, y; cin >> x >> y;
     int d, l; cin >> d >> l;
@@ -32,7 +42,19 @@ int main() {
     int cy = c - y + 1;
     int w = rx * cy;
 
-    cout << (w * pascal(d + l, d)) % 1000000007 << endl;
+    long ans = func(x, y, d, l);
+
+    cout << (w * ans) % MOD << endl;
+}
+
+int main() {
+    std::ifstream in("AtCoder/ABC003/AtCoder社の冬.input");
+    cin.rdbuf(in.rdbuf());
+
+    int t; cin >> t;
+    for (int x = 1; x <= t; ++x) {
+        solve();
+    }
 
     return 0;
 }
