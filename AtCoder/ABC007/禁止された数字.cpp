261a3b2f4fc9f085b77b68a2824e76b679c6b05f
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-05-05 15:09:45 +0900

    Update ABC 007 D
    I read the D section of editorial.
    
    cf. https://qiita.com/pinokions009/items/1e98252718eeeeb5c9ab.md

--- AtCoder/ABC007/禁止された数字.cpp
+++ AtCoder/ABC007/禁止された数字.cpp
@@ -1,33 +1,58 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include <vector>
 // #include <fstream>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::terminate;
+using std::string;
+using std::to_string;
+using std::vector;
 
-void solve() {
-    long A, B; cin >> A >> B;
-    long cnt = 0;
-    for (long i = A; i <= B; ++i) {
-        long quotient = i;
-        while (quotient > 0) {
-            int remainder = quotient % 10;
-            if (remainder == 4 || remainder == 9) {
-                cnt++;
-                break;
+long digitDP(string num) {
+    long dp[100][2][2];
+    for (int digit = 0; digit < 100; ++digit) {
+        for (int smaller = 0; smaller < 2; ++smaller) {
+            for (int appeared = 0; appeared < 2; ++appeared) {
+                dp[digit][smaller][appeared] = 0;
             }
-            quotient /= 10;
         }
     }
-    cout << cnt << endl;
+    dp[0][0][0] = 1;
+
+    vector<int> vec;
+    for (auto n : num) {
+        vec.push_back(n - '0');
+    }
+
+    int l = num.size();
+    for(int digit = 0; digit < l; ++digit){
+        for(int smaller = 0; smaller < 2; ++smaller){
+            for(int appeared = 0; appeared < 2; ++appeared){
+                for(int x = 0; x <= (smaller ? 9 : vec[digit]); ++x){
+                    dp[digit + 1][smaller || x < vec[digit]][appeared || x == 4 || x == 9] += dp[digit][smaller][appeared];
+                }
+            }
+        }
+    }
+
+    return dp[l][0][1] + dp[l][1][1];
+}
+
+void solve() {
+    long A; cin >> A; A--;
+    string B; cin >> B;
+
+    cout << digitDP(B) - digitDP(to_string(A)) << endl;
 }
 
 int main() {
     // std::ifstream in("AtCoder/ABC007/禁止された数字.input");
     // cin.rdbuf(in.rdbuf());
-
+    //
     // int t; cin >> t;
     // for (int x = 1; x <= t; ++x) {
     //     solve();
