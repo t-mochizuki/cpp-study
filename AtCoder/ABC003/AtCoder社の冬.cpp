7caf53837dc8eeb1d4a4593fe1a7e30000d91119
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-05-06 16:32:24 +0900

    Update ABD 003 D
    I read the submission #12696303.
    
    cf. https://atcoder.jp/contests/abc003/submissions/12696303

--- AtCoder/ABC003/AtCoder社の冬.cpp
+++ AtCoder/ABC003/AtCoder社の冬.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <iostream>
-#include <fstream>
+// #include <fstream>
 
 using std::cin;
 using std::cout;
@@ -22,6 +22,8 @@ long pascal(long i, long j) {
 
 long func(long x, long y, long d, long l) {
     if (x < 0 || y < 0) return 0;
+    if (d > x * y) return 0;
+    if (l > (x * y - d)) return 0;
     return pascal(x * y, d) * pascal(x * y - d, l) % MOD;
 }
 
@@ -44,17 +46,59 @@ void solve() {
 
     long ans = func(x, y, d, l);
 
+    if (x * y == d + l) {
+    } else {
+        // A: XY領域のもっとも上の行を使っていない
+        long tmp = func(x - 1, y, d, l);
+        // B: XY領域のもっとも左の列を使っていない
+        tmp += func(x, y - 1, d, l);
+        // C: XY領域のもっとも下の行を使っていない
+        tmp += func(x - 1, y, d, l);
+        // D: XY領域のもっとも右の列を使っていない
+        tmp += func(x, y - 1, d, l);
+
+        // A かつ B
+        tmp -= func(x - 1, y - 1, d, l);
+        // A かつ C
+        tmp -= func(x - 2, y, d, l);
+        // A かつ D
+        tmp -= func(x - 1, y - 1, d, l);
+        // B かつ C
+        tmp -= func(x - 1, y - 1, d, l);
+        // B かつ D
+        tmp -= func(x, y - 2, d, l);
+        // C かつ D
+        tmp -= func(x - 1, y - 1, d, l);
+
+        // A かつ B かつ C
+        tmp += func(x - 2, y - 1, d, l);
+        // A かつ B かつ D
+        tmp += func(x - 1, y - 2, d, l);
+        // A かつ C かつ D
+        tmp += func(x - 2, y - 1, d, l);
+        // B かつ C かつ D
+        tmp += func(x - 1, y - 2, d, l);
+
+        // A かつ B かつ C かつ D
+        tmp -= func(x - 2, y - 2, d, l);
+
+        ans -= tmp;
+        ans += MOD * 7;
+    }
+
     cout << (w * ans) % MOD << endl;
 }
 
 int main() {
-    std::ifstream in("AtCoder/ABC003/AtCoder社の冬.input");
-    cin.rdbuf(in.rdbuf());
+    // std::ifstream in("AtCoder/ABC003/AtCoder社の冬.input");
+    // cin.rdbuf(in.rdbuf());
 
-    int t; cin >> t;
-    for (int x = 1; x <= t; ++x) {
-        solve();
-    }
+    // int t; cin >> t;
+    // for (int x = 1; x <= t; ++x) {
+    //     solve();
+    // }
+
+    solve();
 
     return 0;
 }
