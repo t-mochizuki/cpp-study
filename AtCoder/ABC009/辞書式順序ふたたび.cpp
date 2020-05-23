efe309c93d50dd9a26f85bb5ec8676d5fad0fd6b
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-05-23 18:15:19 +0900

    Update ABC 009 C
    I read the C section of editorial.
    I got WA.

--- AtCoder/ABC009/辞書式順序ふたたび.cpp
+++ AtCoder/ABC009/辞書式順序ふたたび.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <map>
 #include <fstream>
-
-#define DEV 1
 
 using std::cin;
 using std::cout;
@@ -12,11 +11,39 @@ using std::endl;
 using std::terminate;
 using std::string;
 using std::sort;
+using std::map;
+using std::make_pair;
 
-template<class T> inline void swap(T &A, T &B) {
-    T tmp = A;
-    A = B;
-    B = tmp;
+template<class T> inline T min(T X, T Y) {
+    return X < Y ? X : Y;
+}
+
+int count(string S, string T) {
+    map<char, int> sm;
+    for (decltype(S)::iterator it = S.begin(); it != S.end(); ++it) {
+        decltype(sm)::iterator it2 = sm.find(*it);
+        if (it2 != sm.end()) {
+            it2->second++;
+        } else {
+            sm.insert(make_pair(*it, 1));
+        }
+    }
+
+    map<char, int> tm;
+    for (decltype(T)::iterator it = T.begin(); it != T.end(); ++it) {
+        decltype(tm)::iterator it2 = tm.find(*it);
+        if (it2 != tm.end()) {
+            it2->second++;
+        } else {
+            tm.insert(make_pair(*it, 1));
+        }
+    }
+
+    int tmp = 0;
+    for (decltype(tm)::iterator it  = tm.begin(); it != tm.end(); ++it) {
+        tmp += min(sm[it->first], tm[it->first]);
+    }
+    return tmp;
 }
 
 void solve() {
@@ -26,10 +53,58 @@ void solve() {
     string T = S;
     sort(T.begin(), T.end());
 
-    if (N == K) {
-        cout << T << endl;
-    } else {
+    // for (decltype(tm)::iterator it  = tm.begin(); it != tm.end(); ++it) {
+    //     cout << it->first << " " << it->second << endl;
+    // }
+    // cout << endl;
+
+    map<char, int> sm;
+    for (decltype(S)::iterator it = S.begin(); it != S.end(); ++it) {
+        decltype(sm)::iterator it2 = sm.find(*it);
+        if (it2 != sm.end()) {
+            it2->second++;
+        } else {
+            sm.insert(make_pair(*it, 1));
+        }
     }
+
+    map<char, int> tm;
+    for (decltype(T)::iterator it = T.begin(); it != T.end(); ++it) {
+        decltype(tm)::iterator it2 = tm.find(*it);
+        if (it2 != tm.end()) {
+            it2->second++;
+        } else {
+            tm.insert(make_pair(*it, 1));
+        }
+    }
+
+    char ans[N + 1]; ans[N] = '\0';
+    for (int i = 0; i <= N - 1; ++i) {
+        ans[i] = '0';
+        if (N - count(S, T.substr(i)) >= K) {
+            if (sm[S[i]] > 0) {
+                ans[i] = S[i];
+                sm[S[i]]--;
+            }
+        } else {
+            ans[i] = T[i];
+            tm[T[i]]--;
+            sm[T[i]]--;
+        }
+    }
+
+    for (int i = 0; i <= N - 1; ++i) {
+        if (ans[i] == '0') {
+            for (decltype(sm)::iterator it = sm.begin(); it != sm.end(); ++it) {
+                if (it->second > 0) {
+                    ans[i] = it->first;
+                    it->second--;
+                }
+            }
+        }
+    }
+
+    cout << ans << endl;
 }
 
 int main() {
