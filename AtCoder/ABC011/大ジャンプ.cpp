d81c95f3f93dd51bf2728756cf8d1ff68ec12eda
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-05-24 16:55:06 +0900

    Add ABC 011 D
    My score was 90.

--- AtCoder/ABC011/大ジャンプ.cpp
+++ AtCoder/ABC011/大ジャンプ.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <iostream>
-#include <vector>
+#include <cmath>
 #include <fstream>
 
 #define DEV 1
@@ -9,47 +9,58 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::terminate;
-using std::vector;
 
-template<class T> inline T pow(T base, T exponent) {
-    if (exponent == 0) return 1;
+// void dfs(int index, int N, int* route) {
+//     if (index == N) {
+//         for (int i = 0; i < N; ++i) {
+//             if (i == 0) {
+//                 cout << route[i];
+//             } else {
+//                 cout << " " << route[i];
+//             }
+//         }
+//         cout << endl;
+//         return ;
+//     }
+//     for (int i = 0; i < 4; ++i) {
+//         if (i == 0) {
+//             route[index] = 0;
+//             dfs(index + 1, N, route);
+//         } else if (i == 1) {
+//             route[index] = 1;
+//             dfs(index + 1, N, route);
+//         } else if (i == 2) {
+//             route[index] = 2;
+//             dfs(index + 1, N, route);
+//         } else {
+//             route[index] = 3;
+//             dfs(index + 1, N, route);
+//         }
+//     }
+// }
 
-    if (exponent % 2 == 1) {
-        return base * pow(base, exponent - 1);
-    } else {
-        T tmp = pow(base, exponent / 2);
-        return tmp * tmp;
-    }
-}
-
-void dfs(int index, int N, int* route) {
+long dfs(int index, int N, int D, int X, int Y, int posX, int posY) {
     if (index == N) {
-        for (int i = 0; i < N; ++i) {
-            if (i == 0) {
-                cout << route[i];
-            } else {
-                cout << " " << route[i];
-            }
+        if (X == posX && Y == posY) {
+            return 1;
+        } else {
+            return 0;
         }
-        cout << endl;
-        return ;
     }
 
+    long count = 0;
     for (int i = 0; i < 4; ++i) {
         if (i == 0) {
-            route[index] = 0;
-            dfs(index + 1, N, route);
+            count += dfs(index + 1, N, D, X, Y, posX, posY + D);
         } else if (i == 1) {
-            route[index] = 1;
-            dfs(index + 1, N, route);
+            count += dfs(index + 1, N, D, X, Y, posX + D, posY);
         } else if (i == 2) {
-            route[index] = 2;
-            dfs(index + 1, N, route);
+            count += dfs(index + 1, N, D, X, Y, posX, posY - D);
         } else {
-            route[index] = 3;
-            dfs(index + 1, N, route);
+            count += dfs(index + 1, N, D, X, Y, posX - D, posY);
         }
     }
+    return count;
 }
 
 void solve() {
@@ -60,8 +71,7 @@ void solve() {
         terminate();
     }
 
-    int route[N];
-    dfs(0, N, route);
+    cout << dfs(0, N, D, X, Y, 0, 0) * 1.0 * pow(0.25, N) << endl;
 }
 
 int main() {
