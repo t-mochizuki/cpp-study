064266bd7008614632d679448638627ea06c37b4
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-07-28 08:53:56 +0900

    Update ABC 159 D
    I got AC.

--- AtCoder/ABC159/Banned K.cpp
+++ AtCoder/ABC159/Banned K.cpp
@@ -29,15 +29,14 @@ void solve() {
         }
     }
 
+    long s = 0;
+    for (map<int, long>::iterator it = m.begin(); it != m.end(); ++it) {
+        s += (it->second * (it->second - 1)) / 2;
+    }
+
     for (int i = 0; i < n; ++i) {
         long y = 0;
-        for (map<int, long>::iterator it = m.begin(); it != m.end(); ++it) {
-            if (a[i] == it->first) {
-                y += ((it->second - 1) * (it->second - 2)) / 2;
-            } else {
-                y += (it->second * (it->second - 1)) / 2;
-            }
-        }
+        y = s - (m[a[i]] - 1);
         cout << y << endl;
     }
 }
