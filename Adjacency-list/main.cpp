b08c2a963e539b8cb8d3622f00ab782cb913572b
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-08-30 11:26:42 +0900

    To search a graph, add depth first search

--- Adjacency-list/main.cpp
+++ Adjacency-list/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <stack>
 
 #define DEV 1
 
@@ -10,8 +11,45 @@ using std::cout;
 using std::endl;
 using std::terminate;
 using std::vector;
+using std::stack;
 
 typedef vector<vector<int> > Graph;
+typedef vector<int> Edges;
+
+int dfs(Graph G, int N, int v) {
+    vector<bool> visited; visited.assign(N, false);
+
+    stack<int> sta;
+
+    visited[v] = true;
+    sta.push(v);
+
+    while (sta.empty() == false) {
+        int u = sta.top(); sta.pop();
+        for (int x : G[u]) {
+            if (visited[x] == false) {
+                visited[x] = true;
+                sta.push(x);
+            }
+        }
+    }
+
+    cout << v << ": ";
+    int cnt = 0;
+    for (int i = 0; i < N; ++i) {
+        if (visited[i]) {
+            if (cnt == 0) {
+                cout << i;
+            } else {
+                cout << " " << i;
+            }
+            cnt++;
+        }
+    }
+    cout << endl;
+
+    return cnt;
+}
 
 void solve() {
     int N, M; cin >> N >> M;
@@ -20,31 +58,22 @@ void solve() {
     for (int i = 0; i < M; ++i) {
         int a, b; cin >> a >> b;
         G[a].push_back(b);
+        // G[b].push_back(a);
     }
 
-    for (int i = 0; i < N; ++i) {
-        cout << i << ": ";
-        for (vector<int>::iterator it = G[i].begin(); it != G[i].end(); ++it) {
-            if (it == G[i].begin()) cout << *it;
-            else cout << " " << *it;
-        }
-        cout << endl;
-    }
-
-    // for (vector<int> V : G) {
-    //     for (int x : V) {
-    //         cout << x << endl;
-    //     }
-    // }
-
-    // for (Graph::iterator Vi = G.begin(); Vi != G.end(); ++Vi) {
-    //     cout << Vi - G.begin() << ": ";
-    //     for (vector<int>::iterator Vj = Vi->begin(); Vj != Vi->end(); ++Vj) {
-    //         if (Vj == Vi->begin()) cout << *Vj;
-    //         else cout << " " << *Vj;
+    // for (int i = 0; i < N; ++i) {
+    //     cout << i << ": ";
+    //     for (Edges::iterator it = G[i].begin(); it != G[i].end(); ++it) {
+    //         if (it == G[i].begin()) cout << *it;
+    //         else cout << " " << *it;
     //     }
     //     cout << endl;
     // }
+
+    for (int v = 0; v < N; ++v) {
+        dfs(G, N, v);
+    }
+
 }
 
 int main() {
