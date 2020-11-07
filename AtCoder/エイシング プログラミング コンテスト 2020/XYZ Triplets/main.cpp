fcd9f0a181ca2f56c32513e10ca96e6ef2ada81a
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-11-07 21:43:32 +0900

    Update XYZ Triplets

--- AtCoder/エイシング プログラミング コンテスト 2020/XYZ Triplets/main.cpp
+++ AtCoder/エイシング プログラミング コンテスト 2020/XYZ Triplets/main.cpp
@@ -16,20 +16,22 @@ using std::vector;
 using std::map;
 using std::make_pair;
 
-// TODO: WA
-
 void solve() {
     int n; cin >> n;
+
+    int ans[n+1];
+    for (int i = 0; i < n+1; ++i) {
+        ans[i]=0;
+    }
+    for (int x = 1; x <= 100; ++x)
+    for (int y = 1; y <= 100; ++y)
+    for (int z = 1; z <= 100; ++z) {
+        int v = x * x + y * y + z * z + x * y + y * z + z * x;
+        if (v<n+1) ans[v]++;
+    }
+
     for (int i = 1; i <= n; ++i) {
-        int ans = 0;
-        for (int x = 1; x <= 100; ++x)
-        for (int y = 1; y <= 100; ++y)
-        for (int z = 1; z <= 100; ++z) {
-            if (i == x * x + y * y + z * z + x * y + y * z + z * x) {
-                ans++;
-            }
-        }
-        cout << ans << endl;
+        cout << ans[i] << endl;
     }
 }
 
