38a9608775584a2811b2b62b4422d8ef68819cc2
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-11-07 16:45:24 +0900

    Update M-SOLUTIONS プロコンオープン 2020

---
+++ AtCoder/M-SOLUTIONS プロコンオープン 2020/Road to Millionaire/main.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+#define DEV 1
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::terminate;
+using std::string;
+using std::vector;
+using std::map;
+using std::make_pair;
+
+void solve() {
+    int n; cin >> n;
+    int a[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    long ans = 1000;
+    for (int i = 1; i < n; ++i) {
+        if (a[i] > a[i-1]) {
+            ans += (ans / a[i-1]) * (a[i]-a[i-1]);
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+
+#ifdef DEV
+    std::ifstream in("input");
+    cin.rdbuf(in.rdbuf());
+
+    int t; cin >> t;
+    for (int x = 1; x <= t; ++x) {
+        solve();
+    }
+#else
+    solve();
+#endif
+
+    return 0;
+}
