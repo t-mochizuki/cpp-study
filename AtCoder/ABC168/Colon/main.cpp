fff0b7796bab49dd941b4bbb44e6113ef0f71dc0
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-11-08 14:06:55 +0900

    Add ABC 168 C
    I got WA.

--- AtCoder/ABC168/Colon/main.cpp
+++ AtCoder/ABC168/Colon/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 #define DEV 1
 
@@ -9,8 +10,31 @@ using std::cout;
 using std::endl;
 using std::terminate;
 
+// TODO: WA
+
+const double pi = 2.0 * asin(1.0);
+const double radian = pi / 180.0;
+
 void solve() {
-    int a, b, h, m; cin >> a >> b >> h >> m;
+    int b, c;
+    int h; // 0 <= h <= 11
+    int m; // 0 <= m <= 59
+    cin >> b >> c >> h >> m;
+
+    // 余弦定理
+    // cosα=(b^2 + c^2 - a^2) / 2bc
+    // a^2=b^2 + c^2 - 2bc cosα
+
+    double hh = h * (360.0 / 12);
+    double mm = m * (360.0 / 60);
+    double r;
+    if (hh>mm) r=hh-mm;
+    else r=mm-hh;
+    if (r > 180) r-=180;
+    r*=radian;
+
+    double ans = sqrt(b*b+c*c-2*b*c*cos(r));
+    printf("%.15g\n", ans);
 }
 
 int main() {
