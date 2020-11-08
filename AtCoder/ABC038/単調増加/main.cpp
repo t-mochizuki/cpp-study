432ea44c91289e2166d9262aaf39bdd41658bc27
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-11-08 17:07:59 +0900

    Add ABC 038 C
    I got TLE.

---
+++ AtCoder/ABC038/単調増加/main.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+#define DEV 1
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::terminate;
+using std::string;
+using std::vector;
+using std::map;
+using std::make_pair;
+using std::sort;
+using std::to_string;
+using std::lower_bound;
+using std::distance;
+
+void solve() {
+    int n; cin >> n;
+    int a[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i; j < n; ++j) {
+            if (j==i) ans++;
+            else {
+                if (a[j]>a[j-1]) ans++;
+                else break;
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+
+#ifdef DEV
+    std::ifstream in("input");
+    cin.rdbuf(in.rdbuf());
+
+    int t; cin >> t;
+    for (int x = 1; x <= t; ++x) {
+        solve();
+    }
+#else
+    solve();
+#endif
+
+    return 0;
+}
