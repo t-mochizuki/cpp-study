690b0864ffda01d53acf4aff3e2842976f4168d5
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-11-14 21:04:19 +0900

    Update ABC 038 D
    I got AC.

--- AtCoder/ABC038/プレゼント/main.cpp
+++ AtCoder/ABC038/プレゼント/main.cpp
@@ -22,25 +22,62 @@ using std::lower_bound;
 using std::distance;
 using std::max_element;
 
-const int INF = 1 << 30;
-
 template<class T> inline T max(T X, T Y) {
     return X > Y ? X : Y;
 }
 
+// Binary Indexed Tree, BIT, Fenwick Tree
+// cf. https://ikatakos.com/pot/programming_algorithm/data_structure/binary_indexed_tree
+class Fenwick {
+    public:
+
+    int _n;
+    int bit[100005];
+
+    Fenwick(int n): _n(n) {
+        for (int i = 0; i < 100005; ++i) {
+            bit[i] = 0;
+        }
+    }
+
+    void update(int i, int a) {
+        for (int x = i; x <= _n; x += x & -x) bit[x] = max(a, bit[x]);
+    }
+
+    int query(int i) {
+        int ret = 0;
+        for (int x = i; x > 0; x -= x & -x) ret = max(ret, bit[x]);
+        return ret;
+    }
+
+    void print() {
+        for (int i = 0; i < 10; ++i) {
+            if (i == 9) {
+                cout << bit[i] << endl;
+            } else {
+                cout << bit[i] << " ";
+            }
+        }
+    }
+
+};
+
+const int INF = 1 << 30;
+
 class Box {
-public:
+    public:
+
     int _w{INF};
     int _h{INF};
 
     Box() {}
     Box(int w, int h)
         : _w(w), _h(h) {}
-    bool operator<(const Box& rhs) const {
-        if (_w < rhs._w) {
+    bool operator<(const Box& r) const {
+        if (_h<r._h) {
             return true;
-        } else if (_w == rhs._w) {
-            if (_h < rhs._h) {
+        } else if (_h==r._h) {
+            if (_w>r._w) {
                 return true;
             } else {
                 return false;
@@ -61,29 +98,30 @@ void solve() {
         boxes[i]=b;
     }
 
-    if (n>1000) {
-        terminate();
-    }
-
     sort(boxes,boxes+n);
+    // sort(boxes,boxes+n,[](const Box& l,const Box& r) { return l._w>r._w; });
 
     // for (int i = 0; i < n; ++i) {
     //     cout<<boxes[i]._w<<" "<<boxes[i]._h<<endl;
     // }
 
-    int dp[n];dp[0]=0;
-    for (int i=1;i<n;++i) {
+    int dp[n+1];
+    for (int i=0;i<=n;++i) {
         dp[i]=0;
-        for (int j=0;j<i;++j) {
-            if (boxes[i]._w>boxes[j]._w&&boxes[i]._h>boxes[j]._h) {
-                dp[i]=max(dp[i],dp[j]+1);
-            }
-        }
     }
 
+    Fenwick bit(100005);
+    for (int i=1;i<=n;++i) {
+        // wが降順なので、hが同じ箱は入れ子にならない
+        dp[i]=bit.query(boxes[i-1]._w-1)+1;
+        bit.update(boxes[i-1]._w,dp[i]);
+    }
+
+    // bit.print();
+
     int ans=0;
-    ans=*(max_element(dp,dp+n));
-    cout<<ans+1<<endl;
+    ans=*(max_element(dp,dp+n+1));
+    cout<<ans<<endl;
 }
 
 int main() {
