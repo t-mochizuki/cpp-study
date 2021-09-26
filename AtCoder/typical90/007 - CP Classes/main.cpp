8c2aa72a8f47197fac7ecf2752e589d1b75cb0db
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2021-09-26 17:48:40 +0900

    Add typical90 007

---
+++ AtCoder/typical90/007 - CP Classes/main.cpp
@@ -0,0 +1,75 @@
+// g++ -std=c++14 -DDEV=1 main.cpp
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::terminate;
+using std::vector;
+using std::sort;
+using std::abs;
+using std::min;
+
+#define rep(i, a, n) for (int i = (a); i < (n); ++i)
+#define bit(n, k) ((n >> k) & 1)
+
+class Problem {
+private:
+
+    int N, Q;
+    vector<int> a;
+
+public:
+    Problem() {
+        cin >> N;
+
+        a.resize(N);
+        rep(i, 0, N) cin >> a[i];
+
+        sort(a.begin(), a.end());
+
+        cin >> Q;
+    }
+
+    void solve() {
+
+        rep(i, 0, Q) {
+            int b;
+            cin >> b;
+
+            auto it = std::lower_bound(a.begin(), a.end(), b);
+            if (it == a.begin()) {
+                int right = abs(*(it) - b);
+                cout << right << endl;
+            } else {
+                int left = abs(*(it-1) - b);
+                int right = abs(*(it) - b);
+                cout << min(left, right) << endl;
+            }
+        }
+
+    }
+};
+
+int main() {
+
+#ifdef DEV
+    std::ifstream in("input");
+    cin.rdbuf(in.rdbuf());
+
+    int t; cin >> t;
+    for (int x = 1; x <= t; ++x) {
+        Problem p;
+        p.solve();
+    }
+#else
+    Problem p;
+    p.solve();
+#endif
+
+    return 0;
+}
