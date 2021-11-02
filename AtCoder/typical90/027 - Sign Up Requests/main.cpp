e52f60914e8bf35d063ced5629b4c464bbe882ea
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2021-11-02 23:55:24 +0900

    Add typical90 027

--- AtCoder/typical90/027 - Sign Up Requests/main.cpp
+++ AtCoder/typical90/027 - Sign Up Requests/main.cpp
@@ -4,12 +4,16 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <set>
+#include <string>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::terminate;
 using std::vector;
+using std::set;
+using std::string;
 
 #define rep(i, a, n) for (int i = (a); i < (n); ++i)
 #define bit(n, k) ((n >> k) & 1)
@@ -17,12 +21,25 @@ using std::vector;
 class Problem {
 private:
 
+    int N;
+    set<string> S;
+
 public:
 
     Problem() {
+        cin >> N;
+        assert(1 <= N);
+        assert(N <= 100000);
     }
 
     void solve() {
+        rep(i, 1, N+1) {
+            string userName; cin >> userName;
+            if (S.find(userName) == S.end()) {
+                cout << i << endl;
+            }
+            S.insert(userName);
+        }
     }
 };
 
