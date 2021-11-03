cd64185bedb65fe838abdaa3c34994b0c151a191
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2021-11-03 23:20:46 +0900

    Update typical90 029
    I got 2 points.

--- AtCoder/typical90/029 - Long Bricks/main.cpp
+++ AtCoder/typical90/029 - Long Bricks/main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <map>
 
 using std::cin;
 using std::cout;
@@ -11,6 +13,11 @@ using std::endl;
 using std::terminate;
 using std::vector;
 using std::max;
+using std::sort;
+using std::map;
+using std::make_pair;
+
+// キーワード: 「座標圧縮」で効率化
 
 #define rep(i, a, n) for (int i = (a); i < (n); ++i)
 #define bit(n, k) ((n >> k) & 1)
@@ -19,6 +26,8 @@ class Problem {
 private:
 
     int W, N;
+    vector<int> L, R;
+    map<int, int> zip;
 
 public:
 
@@ -29,7 +38,34 @@ public:
         assert(1 <= N);
         assert(N <= 250000);
 
-        if (W > 9000 || N > 9000) {
+        L.resize(N);
+        R.resize(N);
+        rep(i, 0, N) {
+            cin >> L[i] >> R[i];
+
+            assert(1 <= L[i]);
+            assert(W >= L[i]);
+            assert(1 <= R[i]);
+            assert(W >= R[i]);
+            assert(L[i] <= R[i]);
+        }
+
+        rep(i, 0, N) {
+            if (zip.find(L[i]) == zip.end()) {
+                zip.insert(make_pair(L[i], 1));
+            }
+            if (zip.find(R[i]) == zip.end()) {
+                zip.insert(make_pair(R[i], 1));
+            }
+        }
+
+        int num = 1;
+        for (auto it = zip.begin(); it != zip.end(); ++it) {
+            it->second = num;
+            num++;
+        }
+
+        if (N > 9000) {
             terminate();
         }
     }
@@ -38,24 +74,16 @@ public:
         vector<int> height;
         height.assign(W+1, 0);
 
-        rep(x, 0, N) {
-            int L, R; cin >> L >> R;
-
-            assert(1 <= L);
-            assert(L <= W);
-            assert(1 <= R);
-            assert(R <= W);
-            assert(L <= R);
-
+        rep(i, 0, N) {
             int maximum = 0;
-            rep(i, L, R+1) {
-                maximum = max(height[i], maximum);
+            rep(j, zip[L[i]], zip[R[i]]+1) {
+                maximum = max(height[j], maximum);
             }
             maximum++;
 
             cout << maximum << endl;
 
-            rep(i, L, R+1) height[i] = maximum;
+            rep(j, zip[L[i]], zip[R[i]]+1) height[j] = maximum;
         }
     }
 };
