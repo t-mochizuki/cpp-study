f7fd68b7b96c0d052fde4747dc5669d333afa734
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2022-03-29 16:01:52 +0900

    Add typical90 050

---
+++ AtCoder/typical90/050 - Stair Jump/main.cpp
@@ -0,0 +1,75 @@
+// g++ -std=c++17 -D DEV=1 main.cpp
+#include <stdio.h>
+#include <cassert>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+#include <deque>
+#include <tuple>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::terminate;
+using std::vector;
+using std::string;
+using std::map;
+using std::pair;
+using std::deque;
+using std::tuple;
+using std::make_tuple;
+using std::tie;
+using std::abs;
+using std::sort;
+
+#define rep(i, a, n) for (int i = (a); i < (n); ++i)
+#define bit(n, k) ((n >> k) & 1)
+
+class Program {
+private:
+
+    int N, L;
+    vector<int> dp;
+
+public:
+
+    Program() {
+        cin >> N >> L;
+        dp.assign(200005, 0);
+        dp[0] = 1;
+    }
+
+    void solve() {
+        rep(i, 0, N) {
+            for (auto j : {1, L}) {
+                if (i+j <= N) {
+                    dp[i+j] += dp[i];
+                    dp[i+j] %= 1000000007L;
+                }
+            }
+        }
+        cout << dp[N] << endl;
+    }
+};
+
+int main() {
+
+#ifdef DEV
+    std::ifstream in("input");
+    cin.rdbuf(in.rdbuf());
+
+    int t; cin >> t;
+    for (int x = 1; x <= t; ++x) {
+        Program p;
+        p.solve();
+    }
+#else
+    Program p;
+    p.solve();
+#endif
+
+    return 0;
+}
