dce51e34426c9a8251917cc72099ad7432ccb54c
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2024-06-06 09:37:07 +0900

    Add modulo.rb

---
+++ ruby/modulo.rb
@@ -0,0 +1,46 @@
+class Modulo
+  attr_reader :value
+
+  def initialize(x, mod)
+    @mod = mod
+    if x < 0
+      x += mod
+      x %= mod
+    end
+    @value = x
+  end
+
+  def +(other)
+    Modulo.new(value + other.value % @mod, @mod)
+  end
+
+  def -(other)
+    Modulo.new(value - other.value % @mod, @mod)
+  end
+
+  def *(other)
+    Modulo.new(value * other.value % @mod, @mod)
+  end
+
+  def inverse
+    x, y = extendedEuclidean(value, @mod)
+    return x % @mod
+  end
+
+  def /(other)
+    Modulo.new(value * other.inverse % @mod, @mod)
+  end
+
+  private
+
+  def extendedEuclidean(a, b, x = 1, y = 0, u = 0, v = 1)
+    if b == 0
+      return [x, y]
+    else
+      q, r = a.divmod b
+      x -= q * u
+      y -= q * v
+      return extendedEuclidean(b, r, u, v, x, y)
+    end
+  end
+end
